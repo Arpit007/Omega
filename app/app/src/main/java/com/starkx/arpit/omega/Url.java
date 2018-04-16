69af0106bf16ee55899347200824d98acd89a13b
Arpit Bhatnagar <arpit2011@live.com>
2018-04-16 22:44:26 +0530

    Initial Commit

---
+++ app/app/src/main/java/com/starkx/arpit/omega/Url.java
@@ -0,0 +1,13 @@
+package com.starkx.arpit.omega;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Url extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_url);
+	}
+}
